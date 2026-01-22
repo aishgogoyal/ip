e1c2ecd31545f2d9f46268cc330827625de61a9f
Aishwarya Goyal <aishwarya232005@gmail.com>
2026-01-22 10:25:41 +0800

    Add greeting

---
+++ src/main/java/Avo.java
@@ -0,0 +1,8 @@
+public class Avo {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Avo");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+}
